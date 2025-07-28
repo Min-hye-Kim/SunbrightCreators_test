bc5624872008a3bc03f469743c15afe8881c9bf3
Min-hye-Kim <kimilu@ewha.ac.kr>
2025-07-28 19:57:47 +0900

    � GPS 기능 구현

--- GPS_2.js
+++ GPS_2.js
@@ -6,13 +6,13 @@ if ('geolocation' in navigator) {
       console.log("위도:", latitude, "경도:", longitude);
     },
     (error) => {
-      // 에러 발생 시 처리 (예: 거부했거나 실패했을 때)
-      document.getElementById("location").innerText = "위치 정보를 가져올 수 없습니다.";
+      // 에러처리 (e.g. 거부 or 실패)
+      document.getElementById("location").innerText = "위치 정보 가져오기 실패";
       console.error(error);
     }
   );
 } else {
-  // GPS 기능을 지원하지 않는 경우
-  document.getElementById("location").innerText = "이 브라우저에서는 위치 정보 사용이 지원되지 않습니다.";
+      // GPS 기능 지원 X 경우
+  document.getElementById("location").innerText = "브라우저에서 위치정보 사용 지원 X";
 }
 
